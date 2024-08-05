fe87b8c98e21d8094094328ba358ab7508c9fec3
gauravgajjar <gauravgajjar88@gmail.com>
2024-08-05 14:50:24 +0100

    Complete Task 4

---
+++ QuoteColorConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace TakeHomeAssessment
+{
+    public class QuoteColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string quote)
+            {
+                var length = quote.Length;
+
+                if (length % 3 == 0)
+                {
+                    return Colors.Green; // Always green if divisible by 3
+                }
+                if (length < 30)
+                {
+                    return Colors.Black;
+                }
+                if (length >= 30 && length <= 65)
+                {
+                    return Colors.Purple;
+                }
+                if (length > 65 && length < 100)
+                {
+                    return Colors.Red;
+                }
+                return Colors.Blue;
+            }
+            return Colors.Black; // Default color
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Views/QuoteFontStyleConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace TakeHomeAssessment
+{
+    public class QuoteFontStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string quote)
+            {
+                var length = quote.Length;
+
+                if (length % 3 == 0)
+                {
+                    return FontAttributes.Bold;
+                }
+                if (length >= 80)
+                {
+                    return FontAttributes.Italic;
+                }
+                if (length >= 50)
+                {
+                    return FontAttributes.None; // Underline is not directly supported; use a separate property for styling
+                }
+                return FontAttributes.None;
+            }
+            return FontAttributes.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
